63ebfe4ec267269a879fb5575e7060a72c7158e5
mehul1409 <mehulbansalswm1234@gmail.com>
2025-03-29 22:41:38 +0530

    resolve minor bugs

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ dotenv.config()
 const authRoutes = require("./routes/auth.routes")
 const userRoutes = require("./routes/user.routes")
 const adminRoutes = require("./routes/admin.routes")
-const { db } = require("./models/user.model")
+
+const  connectDB  = require("./config/db.js")
 
 // Initialize express app
 const app = express()
@@ -21,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 // Connect to MongoDB
-db();
+connectDB();
 
 // Routes
 app.use("/api/auth", authRoutes)
